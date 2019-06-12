30bd472f7c2be75bbcebbc3b9fa4949c4508dfdc
Ramji Piramanayagam <Ramji.Piramanayagam@vivaenergy.com.au>
2019-06-12 19:31:44 +1000

    Updated packages

---
+++ Roger.Azure.ServiceBus/Configuration/AsbConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roger.Azure.ServiceBus.Configuration
+{
+    public class AsbConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ Roger.Azure.ServiceBus/TopicService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using Roger.Common.Constants;
+using Roger.Common.Extensions;
+using Roger.Json.Extensions;
+
+namespace Roger.Azure.ServiceBus
+{
+    public class TopicService<T>
+    {
+        private readonly ITopicClient Client;
+
+        public TopicService(IAzureServiceBusFactory azureServiceBusFactory, string topicName)
+        {
+            Client = azureServiceBusFactory.CreateTopicClient(topicName);
+        }
+
+        public Task SendAsync(T obj, IDictionary<string, object> properties = null)
+        {
+            var json = obj.ToJson();
+            Message message = new Message(json.GetUtf8Bytes()) {ContentType = ContentTypes.Json};
+            if (properties != null)
+            {
+                foreach (var property in properties)
+                {
+                    message.UserProperties.Add(property);
+                }
+            }
+            return Client.SendAsync(message);
+        }
+    }
+}
